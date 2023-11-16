6101519613419117904ddd737cdfaac543ac87f6
Aprendiz <dannavelhernandez@gmail.com>
2023-11-16 17:30:21 -0500

    vi el tutorial 16-11-23

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from './paginas/auth/login';
+import Pagina1 from './paginas/auth/pagina1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Fragment>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Login/>}/>
+        <Route path="/" exact element={<Pagina1/>}/>
+       
+      </Routes>
+    </Router>
+   </Fragment>
   );
 }
 
---
+++ src/paginas/auth/login.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+const Login = () =>{
+    return (
+        <div className="login-box">
+  <div className="login-logo">
+    <Link to={"../../index2.html"}><b>Iniciar</b>Sesión</Link>
+  </div>
+ 
+  <div className="card">
+    <div className="card-body login-card-body">
+      <p className="login-box-msg">Bienveido, ingresa tus credenciales</p>
+      <form action="../../index3.html" method="post">
+        <div className="input-group mb-3">
+          <input type="email" 
+          className="form-control" 
+          placeholder="Email"
+          id="email"
+          name="email"
+           />
+          <div className="input-group-append">
+            <div className="input-group-text">
+              <span className="fas fa-envelope" />
+            </div>
+          </div>
+        </div>
+        <div className="input-group mb-3">
+          <input type="password"
+          className="form-control"
+           placeholder="Password" 
+           id="password"
+           name="password"
+           />
+          <div className="input-group-append">
+            <div className="input-group-text">
+              <span className="fas fa-lock" />
+            </div>
+          </div>
+        </div>
+        
+          {/* /.col */}
+          </form>
+     
+      <div className="social-auth-links text-center mb-3">
+        
+        <button to={"#"}className="btn btn-block btn-primary">
+           Ingresar
+        </button>
+        <Link to={"#"} className="btn btn-block btn-danger">
+          Crear Cuenta 
+        </Link>
+      </div>
+      {/* /.social-auth-links */}
+      
+    </div>
+    {/* /.login-card-body */}
+  </div>
+</div>
+
+
+    );
+}
+
+export default Login;
